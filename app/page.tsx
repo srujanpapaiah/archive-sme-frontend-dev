8bdf1027181bf60c524b27a80ddb69cf2eb42cf3
Srujan Papaiahgari <srujanpapaiahgari@gmail.com>
2023-09-15 11:35:54 +0530

    added few more packages

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,43 @@
 "use client";
 
-const Home = () => {
-  return (
-    <div>
-      <section className=""></section>
-    </div>
-  );
-};
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-export default Home;
+export default function Home() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [data, setData] = useState({
+    _id: "",
+    username: "",
+    email: "",
+    isVerified: false,
+    role: "",
+    __v: 0,
+  });
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const res = await axios.get(
+          "https://ineuron.ai/_next/data/3tOx5dQnNATDKeswDwkqm/category/NFT.json"
+        );
+        setIsLoggedIn(true);
+        setData(res.data.pageProps.initialState.init);
+        console.log(res.data.pageProps.initialState.init.courses);
+      } catch (error: any) {
+        setIsLoggedIn(false);
+        throw new Error(error.message);
+      }
+    };
+
+    fetchUser();
+  }, []);
+  return (
+    <>
+      {isLoggedIn ? (
+        <h1>Hello your role is {data.role}</h1>
+      ) : (
+        <h1>Welcome {data.courses}</h1>
+      )}
+    </>
+  );
+}
